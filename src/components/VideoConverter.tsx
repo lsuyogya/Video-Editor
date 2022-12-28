e866c7bf8b1ffabc617424e63ebdf83bf7d2983f
Suyogya Luitel <lsuyogya@gmail.com>
2022-12-28 15:18:44 +0545

    ffmpeg core loading and scope fixes

--- src/components/VideoConverter.tsx
+++ src/components/VideoConverter.tsx
@@ -1,21 +1,34 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+import { Brush } from '@visx/brush';
 
 const VideoConverter = () => {
+	const ffmpeg = createFFmpeg({
+		corePath: '../../@ffmpeg/core/dist/ffmpeg-core.js',
+		log: true,
+	});
 	const [inputMedia, setInputMedia] = useState<File>();
 	const [outputMedia, setOutputMedia] = useState<string>();
 	const [loaded, setLoaded] = useState(false);
-	const ffmpeg = createFFmpeg({ log: true });
+	const [ffmpegInstance, setFfmpegInstance] = useState(ffmpeg); //resolves useEffect scope issues
+	const vidRef = useRef(null);
 
 	const handleSubmit = async (e: React.FormEvent) => {
 		e.preventDefault();
+	};
+	const handleConvert = async () => {
 		if (!inputMedia) {
 			alert('Submit a video first');
 			return;
 		}
+		console.log('inside', ffmpegInstance.isLoaded());
 
-		ffmpeg.FS('writeFile', inputMedia.name, await fetchFile(inputMedia));
-		await ffmpeg.run(
+		ffmpegInstance.FS(
+			'writeFile',
+			inputMedia.name,
+			await fetchFile(inputMedia)
+		);
+		await ffmpegInstance.run(
 			'-i',
 			inputMedia.name,
 			'-t',
@@ -26,7 +39,7 @@ const VideoConverter = () => {
 			'gif',
 			`${inputMedia.name}.gif`
 		);
-		const output = ffmpeg.FS('readFile', `${inputMedia.name}.gif`);
+		const output = ffmpegInstance.FS('readFile', `${inputMedia.name}.gif`);
 		const outUrl = URL.createObjectURL(
 			new Blob([output.buffer], { type: 'image/gif' })
 		);
@@ -35,16 +48,13 @@ const VideoConverter = () => {
 
 	const load = async () => {
 		await ffmpeg.load();
+		setFfmpegInstance(ffmpeg);
 		setLoaded(true);
 	};
-	// const load = useCallback(async () => {
-	// 	await ffmpeg.load();
-	// 	setLoaded(true);
-	// }, [loaded]);
 
 	useEffect(() => {
 		load();
-	}, [load]);
+	}, []);
 
 	function handleFileChange(e: React.ChangeEvent<HTMLInputElement>) {
 		const file = e.target.files?.[0];
@@ -56,13 +66,19 @@ const VideoConverter = () => {
 			{loaded ? (
 				<>
 					{inputMedia ? (
-						<video src={URL.createObjectURL(inputMedia)} controls></video>
+						<>
+							<video
+								src={URL.createObjectURL(inputMedia)}
+								ref={vidRef}
+								controls></video>
+							<Brush />
+						</>
 					) : (
 						<div>Submit a video</div>
 					)}
 					<form onSubmit={handleSubmit}>
 						<input type='file' accept='video/*' onChange={handleFileChange} />
-						<button>Convert</button>
+						<button onClick={handleConvert}>Convert</button>
 					</form>
 					{outputMedia && <img src={outputMedia} />}
 				</>
