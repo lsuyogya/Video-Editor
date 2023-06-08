d7f48f499dd2b26ba934a2bd41cf24ff59aa3f00
Suyogya Luitel <lsuyogya@gmail.com>
2023-06-08 23:00:33 +0545

    chore: added /

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 
 export default defineConfig({
-	base: '/Video-Editor',
+	base: '/Video-Editor/',
 	plugins: [react()],
 	server: {
 		headers: {
