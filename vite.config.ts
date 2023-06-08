ea99b37bf3454b54956e5af607aa153c0e3959a1
Suyogya Luitel <lsuyogya@gmail.com>
2023-06-08 22:35:17 +0545

    fix: base test config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '/Video-Editor',
 	plugins: [react()],
 	server: {
 		headers: {
@@ -10,5 +11,4 @@ export default defineConfig({
 			'Cross-Origin-Opener-Policy': 'same-origin',
 		},
 	},
-	base: '/Video-Editor',
 });
